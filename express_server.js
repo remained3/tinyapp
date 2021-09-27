5408aec94ba987ac3e7971963dc59785cee95952
remained3 <sarahthebush@hotmail.com>
2021-09-27 21:51:38 +0000

    added new url page

--- express_server.js
+++ express_server.js
@@ -17,14 +17,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-/* app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-}); */
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
