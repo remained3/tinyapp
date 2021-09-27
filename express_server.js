0a6087535c00d9460f0ae678378ec78e244b89c8
remained3 <sarahthebush@hotmail.com>
2021-09-27 23:57:42 +0000

    added post

--- express_server.js
+++ express_server.js
@@ -7,14 +7,15 @@ app.set("view engine", "ejs")
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-/*app.get("/", (req, res) => {
-  res.send("Hello!");
-}); */
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
