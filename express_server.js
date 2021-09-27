759ab1e0cf275d166a57821ab2c626b2e9a774ce
remained3 <sarahthebush@hotmail.com>
2021-09-27 21:33:15 +0000

    Added header

--- express_server.js
+++ express_server.js
@@ -9,16 +9,26 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send("Hello!");
-});
+}); */
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
+/* app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); */
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
