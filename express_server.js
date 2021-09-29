b868a447cebee431d3b2fc390d5282d84d674534
remained3 <sarahthebush@hotmail.com>
2021-09-29 19:26:28 +0000

    Added login feature

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs")
 
@@ -11,16 +14,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//middleware
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-//allow user to login 
-app.post("/login", (req, res) => {
-  const user = req.body.username
-  res.cookie('username', user)
-  res.redirect("/urls")
-})
 
 //Create a shortened URL and redirects to a page showing the new URL
 app.post("/urls", (req, res) => {
@@ -43,17 +36,27 @@ app.get("/urls.json", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -64,18 +67,15 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
-//update a long URL
-app.post("/urls/:id", (req,res) => {
-  const id = req.params.id;
-  urlDatabase[id] = req.body.quoteContent;
+//allow user to login 
+// Endpoint for the user to signin. Setting a cookie named username.
+app.post("/login", (req, res) => {
+  const user = req.body.username;
+  res.cookie('username', user);
   res.redirect("/urls");
 });
 
 
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
 //RNG for creating a new url
 function generateRandomString() {
@@ -86,3 +86,11 @@ function generateRandomString() {
    }
    return outputString;
 }
+
+//makes server listen
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+
